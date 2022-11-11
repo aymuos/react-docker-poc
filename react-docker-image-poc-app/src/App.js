4434e7e98a67394ed56787b5dfdec6d746c687bf
aymuos <soumyamukherjee@mail.com>
2022-11-11 23:53:12 +0530

    Working code with basic commands for docker to run with react app

---
+++ react-docker-image-poc-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the POC React app to test docker 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          I am glad u found this code in My Github and decided to run it 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
